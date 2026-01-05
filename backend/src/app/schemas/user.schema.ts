45b38140118534f55b9cdc000bd3055b7ace11e9
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-01-06 00:10:33 +0100

    feat: add missing user schema

---
+++ backend/src/app/schemas/user.schema.ts
@@ -0,0 +1,16 @@
+import { Type } from "typebox";
+import { Compile, Validator } from "typebox/compile";
+
+const UserSchema = Type.Object({
+    displayName: Type.String({
+        minLength: 1,
+        maxLength: 64
+    }),
+    email: Type.String({
+        format: "email"
+    })
+});
+
+export const UserValidator: Validator = Compile(UserSchema);
+
+export type UserSchemaType = Type.Static<typeof UserSchema>
