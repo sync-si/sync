74d80766b3f955ad0f3c85121b068767fd6bc76a
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-01-06 21:43:15 +0100

    test: refactors tests to conform to new standard

--- backend/tests/Websocket.test.ts
+++ backend/tests/Websocket.test.ts
@@ -8,8 +8,22 @@ beforeAll(() => {
 });
 
 describe("WebSocket", () => {
-    test("connection", done => {
-        const client: WebSocket = new WebSocket("ws://localhost:3001/api/v1/connect/test-room");
+
+    test("connection", async done => {
+        const joinResponse: Response = await fetch("http://localhost:3001/v1/room/test-room/join", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                displayName: "John Doe",
+                email: "john.doe@example.com"
+            })
+        });
+        const data: any = await joinResponse.json();
+        const sessionID: string = data.sessionID;
+
+        const client: WebSocket = new WebSocket(`ws://localhost:3001/v1/connect/${sessionID}`);
         client.onopen = () => {
             client.close();
             done();
@@ -19,52 +33,4 @@ describe("WebSocket", () => {
             done();
         }
     });
-
-    test("join", done => {
-        const client: WebSocket = new WebSocket("ws://localhost:3001/api/v1/connect/test-room");
-        const messageID = crypto.randomUUID();
-        client.onopen = () => {
-            client.send(JSON.stringify({
-                messageID: messageID,
-                payloadID: "https://sync.si/schemas/event/clientJoin",
-                payload: {
-                    displayName: "John Doe",
-                    email: "john.doe@example.com"
-                }
-            }));
-        }
-
-        client.onmessage = message => {
-            expect(JSON.parse(message.data)).toMatchObject({
-                payloadID: "https://sync.si/schemas/event/joinReply",
-                replyTo: messageID,
-                payload: {
-                    displayName: "John Doe",
-                    gravatarHash: "836f82db99121b3481011f16b49dfa5fbc714a0d1b1b9f784a1ebbbf5b39577f"
-                }
-            });
-            client.close();
-            done();
-        }
-    });
-
-    test("message-before-join", done => {
-        const client: WebSocket = new WebSocket("ws://localhost:3001/api/v1/connect/test-room");
-        const messageID = crypto.randomUUID();
-        client.onopen = () => {
-            client.send(JSON.stringify({
-                messageID: messageID,
-                payloadID: "https://sync.si/schemas/chat/clientChat",
-                payload: {
-                    rawMessage: "Hello World!"
-                }
-            }))
-        }
-
-        client.onclose = event => {
-            expect(event.code).toBe(1002);
-            expect(event.reason).toBe("You must first identify yourself with a clientJoin message, before sending anything else!")
-            done();
-        }
-    })
 });
